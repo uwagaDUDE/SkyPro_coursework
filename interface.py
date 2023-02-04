659305de11d4fa4e97d1c961e6f88130bdc1a416
uwaga <puvir00@gmail.com>
2023-02-05 00:07:49 +0300

    stable version without tests

--- interface.py
+++ interface.py
@@ -4,25 +4,31 @@ class Interface:
         return print(f'### История банковских операций ###')
     def operations_count(self, count=0):
         return print(f'Всего операций: {count}\n'
-                     f'Чтобы показать последние 5 операции введите: "last"')
+                     f'Чтобы показать 5 последних операций введите: "last"')
 
     def find_operaiton(self):
         return input('Номер или ID операции: ')
 
     def description_of_operation(self, description):
-        return f"{description}"
+        return f"Тип операции: {description}"
 
     def date_of_operation(self, date):
         return f"Время операции: {date:%d.%m.%Y %H:%M:%S}"
 
-    def operation_from(self, op_from):
-        return f"Перевод от: {op_from}"
+    def operation_from_card(self, card, card_type, card_number):
+        if "Счет" in card:
+            return f"Перевод от: {card_type} {card_number}"
+        else:
+            return f"Перевод от: {card_type} **** **** **** {card_number}"
 
     def operation_to(self, op_to):
         return f"Перевод кому: {op_to}"
 
     def amount(self, amount):
-        return f'Сумма перевода {amount}'
+        return f'Сумма перевода: {amount}'
+
+    def open_new_wallet(self, wallet):
+        return f'Номер счета: {wallet}'
 
 class Errors:
     def wrong_id(self):
--- scripts.py
+++ scripts.py
@@ -18,46 +18,81 @@ class Data:
                     self.ids.append(i['id'])
         self.counts = len(self.ids)
 
-    # def all_operations_count(self):
-    #     """
-    #     Для вывода общего количества транзакций
-    #     :return:
-    #     """
-    #     self.ids = []
-    #     for i in self.operations_dict:
-    #         for x in i:
-    #             if x == 'id':
-    #                 self.ids.append(i)
-    #     return len(self.ids)
 
     def user_search(self, user_input):
-
+        """
+        Обработка запроса пользователя по ID или по номеру транзакции.
+        :param user_input: пользовательский ввод.
+        :return:
+        """
         if user_input.isnumeric():
-            if int(user_input) not in range(1,100) and int(user_input) not in self.ids:
+            if int(user_input) not in range(1,self.counts) and int(user_input) not in self.ids:
                 return print(f'{error.wrong_id()}')
 
             else:
                 if len(user_input) > 3:
                     for i in self.operations_dict:
-                        for x in i:
-                            if int(user_input) == i['id']:
-                                self.information(i)
-                                return
+                        if int(user_input) == i['id']:
+                            self.information(i)
+                            return
 
                 if len(user_input) < 4:
-                    return print(f'{self.ids[int(user_input)-1]}')
+                    for i in self.operations_dict:
+                        if self.ids[int(user_input)-1] == i['id']:
+                            self.information(i)
+                            return
+
         else:
             if user_input == "last":
-                return print(f'{self.ids[0:6]}')
+                ticker = 0
+                for i in self.operations_dict:
+                    if self.ids[ticker] == i['id']:
+                        self.information(i)
+                        ticker += 1
+                        if ticker == 5:
+                            return
+
+            else:
+                return error.wrong_id()
 
     def information(self, i):
-        self.description = i['description']
-        number_of_card = str(i['from'])
-        splitted = number_of_card.split()
+
+        self.description = interface.description_of_operation(i['description'])
+
         date = i['date']
         self.date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f')
-        return print(f'\n{interface.date_of_operation(self.date)} || {self.description}\n'
-                     f'{interface.operation_from(splitted)} -> {interface.operation_to(i["to"])}\n'
-                     f'{interface.amount(i["operationAmount"]["amount"])} {i["operationAmount"]["currency"]["name"]}')
+
+        if "from" in i:
+            number_of_card = i['from']
+            card = number_of_card.split()
+
+            if "Счет" in card:
+                card_number = card[1]
+                card_type = card[0]
+                return self.return_fuction(card, card_type, card_number, i)
+
+            elif len(card) == 2:
+                card_number = card[1][12:]
+                card_type = card[0]
+                return self.return_fuction(card, card_type, card_number, i)
+
+            else:
+                card_number = card[2][12:]
+                card_type=''
+                for c in card[0:2]:
+                    card_type += c
+                return self.return_fuction(card, card_type, card_number, i)
+
+        else:
+            card = i['to'].split()
+            card_number = card[1]
+            return print(f'\n{interface.date_of_operation(self.date)} || {self.description}\n'
+                         f'{interface.open_new_wallet(card_number)}')
+
+    def return_fuction(self, card, card_type, card_number, i):
+
+        print(f'\n{interface.date_of_operation(self.date)} || {self.description}\n'
+              f'{interface.operation_from_card(card, card_type, card_number)} -> {interface.operation_to(i["to"])}\n'
+              f'{interface.amount(i["operationAmount"]["amount"])} {i["operationAmount"]["currency"]["name"]}')
 
 
